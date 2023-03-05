cd7c0948251e10adce10b8a82de80f3944765f79
Mugdho <saha2171515@stud.kuet.ac.bd>
2023-03-05 19:01:16 +0600

    05/03/2023

--- includes/footer.php
+++ includes/footer.php
@@ -26,33 +26,8 @@
         <div class="container">
             <div class="footer-copyright">
                 <div class="row align-items-center">
-                    <div class="col-xl-5 col-lg-5 col-md-5 col-12 aos-item" data-aos="fade-up"
-                         data-aos-duration="500" data-aos-once="true">
-                        <div class="footer-copyright-text md-txt-left lg-txt-left xs-txt-center sm-txt-center">
-                            <p>© 2023 <span class="color-primary">Next Gen Tech</span>, All Right Reserved.</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-7 col-lg-7 col-md-7 col-12 aos-item" data-aos="fade-up"
-                         data-aos-duration="600" data-aos-once="true">
-                        <div class="footer-copyright-nav md-txt-right lg-txt-right xs-txt-center sm-txt-center md-mrt-0 xs-mrt-20 sm-mrt-20">
-                            <a href="#home">Home</a>
-                            <a href="#about_us">About Us</a>
-                            <a href="#serices">Services</a>
-                            <a href="#contact">Contact Us</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-copyright-logo-area">
-                <div class="row align-items-center">
-                    <div class="col-xl-4 col-lg-4 col-md-4 col-12 aos-item" data-aos="fade-up"
-                         data-aos-duration="700" data-aos-once="true">
-                        <div class="footer-copyright-logo mrt-30 md-txt-left lg-txt-left xs-txt-center sm-txt-center">
-                            <a class="btn btn-primary rounded-pill" href="tel:+852 95509806">+852 95509806</a>
-                        </div>
-                    </div>
-                    <div class="col-xl-8 col-lg-8 col-md-8 col-12 aos-item" data-aos="fade-up"
-                         data-aos-duration="800" data-aos-once="true">
+                    <div class="col-12 text-center">
+                        <p style="font-weight: 700"><span class="text-gradient"><a href="tel:85295509806">Contact: +852 95509806</a> © 2023 Next Gen Tech, All Right Reserved.</span></p>
                     </div>
                 </div>
             </div>
